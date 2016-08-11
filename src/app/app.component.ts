da89a7656120d867eebb35352988b3edafe866c9
Reiss Jarvis <reissefer@gmail.com>
2016-08-11 12:51:24 +0100

    remove extra comma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     moduleId: module.id,
     selector: 'app-root',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css'],
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
     title = 'app works!';
